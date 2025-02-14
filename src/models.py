d0b6dc02856bba7920b81465a10750e6edcae091
Luis Malizia <luisfmaliziam@gmail.com>
2025-02-14 11:20:45 +0000

    terminado

--- src/models.py
+++ src/models.py
@@ -1,29 +1,58 @@
 import os
 import sys
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
-from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
 from eralchemy2 import render_er
+from typing import List
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(nullable=False, unique=True)
+  
+    favorite: Mapped[List["Favorite"]] = relationship()
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id : Mapped[int] = mapped_column(ForeignKey('user.id'))
+    people_id : Mapped[int] = mapped_column(ForeignKey('people.id'))
+    planets_id : Mapped[int] = mapped_column(ForeignKey('planets.id'))
+    starships_id: Mapped[int] = mapped_column(ForeignKey('starships.id'))
+    # type : Mapped[str] = mapped_column(nullable=False)
+    # user = relationship('User', back_populates='favorites')
+
+class People(Base):
+    __tablename__ = 'people'
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
+    gender : Mapped[str] = mapped_column(nullable=False)
+    eye_color : Mapped[str] = mapped_column(nullable=False)
+    skin_color : Mapped[str] = mapped_column(nullable=False)
+    favorite: Mapped[List["Favorite"]] = relationship()
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planets(Base):
+    __tablename__ = 'planets'
     id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False)
+    papulation: Mapped[str] = mapped_column(nullable=False)
+    terrain: Mapped[str] = mapped_column(nullable=False)
+    climate: Mapped[str] = mapped_column(nullable=False)
+    favorite: Mapped[List["Favorite"]] = relationship()
 
-    def to_dict(self):
-        return {}
+
+class Starships(Base):
+    __tablename__ = 'starships'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    model: Mapped[str] = mapped_column(nullable=False)
+    passanger: Mapped[str] = mapped_column(nullable=False)
+    cargo_capacity: Mapped[str] = mapped_column(nullable=False)
+    favorite: Mapped[List["Favorite"]] = relationship()
+
+    
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
